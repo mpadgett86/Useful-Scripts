6d9c11fa38c100d79cebfc13a465ed26ee53b353
mpadgett86 <53833359+mpadgett86@users.noreply.github.com>
2019-09-13 17:05:22 -0400

    Add files via upload

---
+++ Get-LaptopModelSKU.ps1
@@ -0,0 +1,11 @@
+﻿$sku = "((Get-WmiObject -Namespace root\wmi -Class MS_SystemInformation | Select-Object -ExpandProperty SystemSKU) -as [string])"
+
+$z = ($sku.Substring($sku.IndexOf(' ') + 1))
+$y = $z.IndexOf(' ')
+
+if ($y -gt 0) {
+    $model = $z.Substring(0, $y)
+} else { $model = $z }
+
+write-host $model
+
